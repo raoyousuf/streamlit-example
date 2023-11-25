c02a8348aa3a36fac96ff2500712c6d12bc941b2
raoyousuf <151982488+raoyousuf@users.noreply.github.com>
2023-11-25 11:20:16 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,26 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
+# Import necessary libraries
+import nltk
+from nltk import ngrams
+import gradio as gr
 
-"""
-# Welcome to Streamlit!
+# Download NLTK data
+nltk.download('punkt')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Function to generate n-grams from text
+def generate_ngrams(text, n):
+    tokens = nltk.word_tokenize(text)
+    n_grams = list(ngrams(tokens, n))
+    return [' '.join(gram) for gram in n_grams]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Gradio Interface
+iface = gr.Interface(
+    fn=generate_ngrams,
+    inputs=gr.Textbox(),
+    outputs=gr.Textbox(),
+    live=True,
+    title="N-gram Extractor",
+    description="Enter a text passage, and the application will output the n-grams.",
+)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Launch the Gradio interface
+iface.launch()
